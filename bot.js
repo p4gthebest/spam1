e4e3186a0026ebe173d3b8707276b4b8fc9485af
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-17 14:14:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+console.log("P4G the best");
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-
-
-
-
-
-client.on('message', message => {
-    if(message.content === 'حيدر'){
-        setInterval(function() {
-            message.channel.send(`**Road To 100 Credit**`);
-            }, 25).then.message.channel.send(`**let see how much credit we have.**`)
-
-}});
+client.on("ready", () => {
+let channel =     client.channels.get("513309819612954624")
+setInterval(function() {
+channel.send(`**Road To 100k Credit**`);
+}, 25)
+})
 
 client.on('message', message => {
-    if(message.content === '**let see how much credit we have.**'){
+    if(message.content === 'check'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '#daily'){
+    if(message.content === 'check'){
         message.channel.send('#credit')
     }
 });
 
-
-client.on('message', message => {
-    if(message.content === '#credit'){
-        message.channel.send('حيدر')
-    }
-});
-
-
-            
 
 
 
